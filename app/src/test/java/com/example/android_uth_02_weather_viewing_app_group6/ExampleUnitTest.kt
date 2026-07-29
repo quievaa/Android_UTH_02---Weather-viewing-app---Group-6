0dfec75e588e5da4bffaa3279ad90f48e24c8bc9
DangKhoa2412 <dangkhoaho2412@gmail.com>
2026-07-30 00:21:25 +0700

    Initial commit

---
+++ app/src/test/java/com/example/android_uth_02_weather_viewing_app_group6/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.android_uth_02_weather_viewing_app_group6
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
