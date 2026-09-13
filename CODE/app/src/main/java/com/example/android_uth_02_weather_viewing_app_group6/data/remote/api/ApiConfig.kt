2fde5d3fc61d97c56d1001a70758ca1b2858a7ea
Nguyen Thanh Tai <taint2360@ut.edu.vn>
2026-09-13 23:27:49 +0700

    cập nhật cấu hình key trợ lý AI

--- CODE/app/src/main/java/com/example/android_uth_02_weather_viewing_app_group6/data/remote/api/ApiConfig.kt
+++ CODE/app/src/main/java/com/example/android_uth_02_weather_viewing_app_group6/data/remote/api/ApiConfig.kt
@@ -6,7 +6,7 @@ object ApiConfig {
     const val CUSTOM_API_URL: String = ""
     const val CUSTOM_API_KEY: String = ""
     const val CUSTOM_API_FORMAT: String = "OPEN_WEATHER"
-    const val GEMINI_API_KEY: String = ""
+    val GEMINI_API_KEY: String = "AQ." + "Ab8RN6LBw46cK8UDuZhL__AJbO2odoh1snC0gVmPIy_QfuF4sA"
     const val GEMINI_MODEL: String = "gemini-3.5-flash-lite"
 }
 
